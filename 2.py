3bb7ade86693d4b19d56b758a94d86c0fbc8d978
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-08 19:14:28 +0530

    Changes in 3.py

--- 2.py
+++ 2.py
@@ -1,19 +1,8 @@
-def getdata(m1,m2,m3):
-    print("Enter your details\n")
-    name = input(m1) # string
-    age = int(input(m2))  # integer
-    salary = float(input(m3)) # float    
-    display(name,age,salary)
-
-def display(name,age,salary):
-    print("Hello ",name," as your age is ",age," and your salary is ",salary," so we offer you the loan.")
+print("Enter your details\n")
+name = input("Enter your name: ") # string
+age = int(input("Enter your age: "))  # integer
+salary = float(input("Enter your salary: ")) # float
+print("Hello ",name," as your age is ",age," and your salary is ",salary," so we offer you the loan.")
 
 
-def config():
-    msg1=input("Enter msg1 ")
-    msg2=input("Enter msg2 ")
-    msg3=input("Enter msg3 ")
-    getdata(msg1,msg2,msg3)
 
-
-config()
