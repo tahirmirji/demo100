673cd86f0fccab67f98f25988cc192311d0444d0
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2023-02-27 00:45:45 +0530

    .

--- datatypes.py
+++ datatypes.py
@@ -9,15 +9,11 @@
 
 
 list1 = []
-
 n = int(input("Enter the n value: "))
-
 print("Enter the elements: ")
 for i in range(n):
     list1.append(int(input()))
 n=len(list1)
-
-
 for i in range(n-1,0,-1):
     for j in range(i):
         if(list1[j] > list1[j+1]):
